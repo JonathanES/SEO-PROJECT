03a25e56dbb1eed7d9c85c60180870357449e243
Jonathan Espiard <jonathan.espiard@epita.fr>
2018-06-28 23:08:45 +0200

    [ADD] Added function for the ngram

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const fs = require('fs');
 
+function generateNgrams(N, words) {
+    return new Promise((resolve, reject) => {
+        const ngramList = [];
+        for (let k = 0; k < (words.length - N + 1); k++) {
+            let s = "";
+            const start = k;
+            const end = k + N;
+            for (let j = start; j < end; j++) {
+                s = s + " " + words[j];
+            }
+            ngramList.push(s);
+        }
+        resolve(ngramList);
+    });
+}
+
+
 const files = fs.readdirSync(__dirname + '/files');
 files.forEach(element => {
     fs.readFile(__dirname + '/files/' + element, "utf8", (err, data) => {
         if (err) throw err;
-        console.log(data);
+        const N = [1, 2, 3, 4, 5]
         const wordsRegexp = /[a-zA-Z']+/g;
-        const words = new Map();
+        const words = [];
         data.replace(wordsRegexp, function (word) {
-            const n = words.get(word);
-            if (n) {
-                words.set(word, 1 + n);
-            } else {
-                words.set(word, 1);
-            }
+            words.push(word);
         });
-        console.log(words);
-      });
+        N.forEach(element => {
+            const promise = generateNgrams(element, words);
+            promise.then(ngramList => {
+                var counts = {};
+                ngramList.forEach(x => { counts[x] = (counts[x] || 0) + 1; });
+                console.log("count ngram " + JSON.stringify(counts));
+            })
+        });
+
+    });
 });
 
 
