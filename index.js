cd419d479dcfa505458373ba20e88ce8cbd83fc8
Jonathan Espiard <jonathan.espiard@epita.fr>
2018-06-28 20:13:49 +0200

    [ADD] Read files

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
 
-console.log('toto');
+const files = fs.readdirSync(__dirname + '/files');
+files.forEach(element => {
+    fs.readFile(__dirname + '/files/' + element, "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
+});
+
+
 
