20d6b84a6530e44f2646c6e8a5df5adc7cc34979
Jonathan Espiard <jonathan.espiard@epita.fr>
2018-06-28 21:47:01 +0200

    [ADD] Map of words and their number of occurence

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ files.forEach(element => {
     fs.readFile(__dirname + '/files/' + element, "utf8", (err, data) => {
         if (err) throw err;
         console.log(data);
+        const wordsRegexp = /[a-zA-Z']+/g;
+        const words = new Map();
+        data.replace(wordsRegexp, function (word) {
+            const n = words.get(word);
+            if (n) {
+                words.set(word, 1 + n);
+            } else {
+                words.set(word, 1);
+            }
+        });
+        console.log(words);
       });
 });
 
