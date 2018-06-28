dadd8c763bbe08f41ef96602f59d286aca178aff
Jonathan Espiard <jonathan.espiard@epita.fr>
2018-06-29 00:52:46 +0200

    [ADD] Working on the implementation of the IDF

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const math = require('mathjs')
 
 function generateNgrams(N, words) {
     return new Promise((resolve, reject) => {
@@ -8,7 +9,10 @@ function generateNgrams(N, words) {
             const start = k;
             const end = k + N;
             for (let j = start; j < end; j++) {
-                s = s + " " + words[j];
+                if (j === start)
+                    s += words[j];
+                else
+                    s = s + " " + words[j];
             }
             ngramList.push(s);
         }
@@ -17,27 +21,66 @@ function generateNgrams(N, words) {
 }
 
 
-const files = fs.readdirSync(__dirname + '/files');
-files.forEach(element => {
-    fs.readFile(__dirname + '/files/' + element, "utf8", (err, data) => {
-        if (err) throw err;
-        const N = [1, 2, 3, 4, 5]
-        const wordsRegexp = /[a-zA-Z']+/g;
-        const words = [];
-        data.replace(wordsRegexp, function (word) {
-            words.push(word);
+/**
+ * @param docs list of list of strings represents the dataset
+ * @param term String represents a term
+ * @return the inverse term frequency of term in documents
+ */
+function idf(docs, term) {
+    return new Promise((resolve, reject) => {
+        let n = 0;
+        let count = 0;
+        docs.forEach(doc => {
+            doc.forEach(word => {
+                if (term === word)
+                    n++;
+            });
+            count++;
         });
-        N.forEach(element => {
-            const promise = generateNgrams(element, words);
-            promise.then(ngramList => {
-                var counts = {};
-                ngramList.forEach(x => { counts[x] = (counts[x] || 0) + 1; });
-                console.log("count ngram " + JSON.stringify(counts));
-            })
-        });
-
+        if (count === docs.length)
+            resolve(math.log((docs.length / n)));
     });
-});
+}
 
 
+function getDocuments() {
+    return new Promise((resolve, reject) => {
+        const files = fs.readdirSync(__dirname + '/files');
+        const documents = [];
+        files.forEach(element => {
+            fs.readFile(__dirname + '/files/' + element, "utf8", (err, data) => {
+                if (err) throw err;
+                // const N = [1, 2, 3, 4, 5]
+                const N = [1];
+                const wordsRegexp = /[a-zA-Z']+/g;
+                const words = [];
+                data.replace(wordsRegexp, function (word) {
+                    words.push(word);
+                });
+                N.forEach(element => {
+                    const promise = generateNgrams(element, words);
+                    promise.then(ngramList => {
+                        documents.push(ngramList);
+                        if (files.length === documents.length)
+                            resolve(documents);
+                    })
+                });
+            });
+        });
+    });
+}
+
+function tfIdf() {
+    const promise = getDocuments();
+    promise.then(documents => {
+        const counts = {};
+        documents[0].forEach(x => { counts[x] = ((counts[x] || 0) + 1) / documents[0].length; });
+        // console.log("count ngram " + JSON.stringify(counts));
+        const idfResult = idf(documents, "ipsum");
+        idfResult.then(res => {
+            console.log(counts.ipsum * res);
+        })
+    });
+}
+tfIdf();
 
